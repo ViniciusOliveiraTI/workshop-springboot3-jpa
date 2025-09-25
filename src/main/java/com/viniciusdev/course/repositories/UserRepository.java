093104ff30215baf0b2f52b9af68021b8f15de81
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-09-25 15:39:10 -0300

    Service layer, component registration

--- src/main/java/com/viniciusdev/course/repositories/UserRepository.java
+++ src/main/java/com/viniciusdev/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.viniciusdev.course.repositories;
 
 import com.viniciusdev.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+// @Repository (opcional)
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
