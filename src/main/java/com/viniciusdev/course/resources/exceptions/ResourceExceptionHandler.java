0fb29e0b0c157367aa21e0f02bf9e9b1b357728f
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-09-30 08:52:37 -0300

    Exception handling - delete

--- src/main/java/com/viniciusdev/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/viniciusdev/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.viniciusdev.course.resources.exceptions;
 
+import com.viniciusdev.course.services.exceptions.DatabaseException;
 import com.viniciusdev.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -24,4 +25,16 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+        String error = "Database error";
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+
+    }
 }
--- src/main/java/com/viniciusdev/course/services/UserService.java
+++ src/main/java/com/viniciusdev/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.viniciusdev.course.services;
 
 import com.viniciusdev.course.entities.User;
 import com.viniciusdev.course.repositories.UserRepository;
+import com.viniciusdev.course.services.exceptions.DatabaseException;
 import com.viniciusdev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+           throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User object) {
